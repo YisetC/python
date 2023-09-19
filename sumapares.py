1693647d0828c8aad53d3a8a1860d60744728de5
YisetC <142763227+YisetC@users.noreply.github.com>
2023-09-18 21:51:36 -0500

    Update sumapares.py

--- sumapares.py
+++ sumapares.py
@@ -1,1 +1,12 @@
-
+print("ingrese el numero inicial")
+i= int(input())
+print("ingrese el numero final")
+f = int(input())
+suma=0
+print("**los numeros pares del rango**")
+while i <= f:
+    if i % 2 == 0:
+        print(i)
+        suma = suma+i
+    i+=1
+print("la suma de los numeros es:",suma)
